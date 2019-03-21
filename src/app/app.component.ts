688c417d0918cd6e9317fe9e7c3a7509ceb28f4d
Sharis <sarezas@gmail.com>
2019-03-21 18:47:57 +0200

    second commit

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,37 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.css']
 })
-export class AppComponent {
+export class AppComponent implements OnInit {
   title = 'parallax';
+  pageYOffset = window.scrollY;
+  constructor() {}
+
+  ngOnInit() {
+    window.addEventListener('scroll', this.doStuff);
+  }
+
+  doStuff() {
+    // console.log(`scrolled ${this.pageYOffset} px from top`);
+    const layers = document.querySelectorAll('[data-type="parallax"]');
+
+    for (const layer of layers as any) {
+      const depth = layer.getAttribute('data-depth');
+      // console.log(depth);
+      const movement = -this.pageYOffset * depth;
+      // console.log(movement);
+      const translate3d = 'translate3d(0, ' + movement + 'px, 0)';
+      layer.style['-webkit-transform'] = translate3d;
+      layer.style['-moz-transform'] = translate3d;
+      layer.style['-ms-transform'] = translate3d;
+      layer.style['-o-transform'] = translate3d;
+      layer.style.transform = translate3d;
+    }
+  }
 }
+
+// -------------------------------------------------------------------------
+
