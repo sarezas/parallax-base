10ab0459c2ec9f94ac0037d1e8fe63e3a49ca7d8
Sharis <sarezas@gmail.com>
2019-03-30 15:18:18 +0200

    filter activity and sort by name implemented

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -8,7 +8,7 @@ export interface Contact {
   surname: string;
   city: string;
   email: string;
-  phome: string;
+  phone: string;
   active: boolean;
 }
 
@@ -20,88 +20,81 @@ export interface Contact {
 export class AppComponent implements OnInit {
   title = 'parallax';
   pageYOffset = window.scrollY;
-  @Input() cl = contacts;
+  @Input() cl: Contact[] = contacts.slice();
+  // @ViewChild('cl') ctli: ElementRef;
   @Input() al = this.cl.filter(c => c.active === true);
-  // @Input() ;
   isChecked = false;
+  order = false;
   @ViewChild('checkbox') checkbox: ElementRef;
 
   constructor() {}
 
   ngOnInit() {
     window.addEventListener('scroll', this.doStuff);
+    console.log(this.toggleActivity);
+
   }
 
   sortByName() {
+    this.order = !this.order;
     const aLSorted = this.al.sort((a: { name: string }, b: { name: string }) => {
-      const aLnameA = a.name.toUpperCase();
-      const aLNameB = b.name.toUpperCase();
-      if (aLnameA < aLNameB) { return -1; }
-      if (aLnameA > aLNameB) { return 1; }
-      return 0;
-    });
+      const x = a.name.toLowerCase();
+      const y = b.name.toLowerCase();
+      switch (this.order) {
+        case x > y:
+          return 1;
+          break;
+        case x < y:
+          return -1;
+          break;
+    }});
 
     const clSorted = this.cl.sort((a: { name: string }, b: { name: string }) => {
-      const cLNameA = a.name.toUpperCase();
-      const cLNameB = b.name.toUpperCase();
-      if (cLNameA < cLNameB) { return -1; }
-      if (cLNameA > cLNameB) { return 1; }
-      return 0;
-    });
+      const x = a.name.toLowerCase();
+      const y = b.name.toLowerCase();
+      switch (this.order) {
+        case x > y:
+          return 1;
+          break;
+        case x < y:
+          return -1;
+          break;
+    }});
 
     if (this.checkbox.nativeElement.checked === true) {
-      this.al = aLSorted;
+      this.cl = aLSorted;
     } else {
       this.cl = clSorted;
     }
-  }
-
-  sortBySurname() {
-    const aLSorted = this.al.sort((a: { surname: string }, b: { surname: string }) => {
-      const aLSurnameA = a.surname.toUpperCase();
-      const aLSurnameB = b.surname.toUpperCase();
-      if (aLSurnameA < aLSurnameB) { return -1; }
-      if (aLSurnameA > aLSurnameB) { return 1; }
-      return 0;
-    });
-
-    const clSorted = this.cl.sort((a: { surname: string }, b: { surname: string }) => {
-      const cLSurnameA = a.surname.toUpperCase();
-      const cLSurnameB = b.surname.toUpperCase();
-      if (cLSurnameA < cLSurnameB) { return -1; }
-      if (cLSurnameA > cLSurnameB) { return 1; }
-      return 0;
-    });
-
-    if (this.checkbox.nativeElement.checked === true) {
-      this.al = aLSorted;
-    } else {
-      this.cl = clSorted;
-    }
-  }
-
-  sortByCity() {
-    const aLSorted = this.al.sort((a: { city: string }, b: { city: string }) => {
-      const aLcityA = a.city.toUpperCase();
-      const aLCityB = b.city.toUpperCase();
-      if (aLcityA < aLCityB) { return -1; }
-      if (aLcityA > aLCityB) { return 1; }
-      return 0;
-    });
-
-    const clSorted = this.cl.sort((a: { city: string }, b: { city: string }) => {
-      const cLCityA = a.city.toUpperCase();
-      const cLCityB = b.city.toUpperCase();
-      if (cLCityA < cLCityB) { return -1; }
-      if (cLCityA > cLCityB) { return 1; }
-      return 0;
-    });
-
-    if (this.checkbox.nativeElement.checked === true) {
-      this.al = aLSorted;
-    } else {
-      this.cl = clSorted;
-    }
+    // if (this.checkbox.nativeElement.checked === true) {
+    //   list.sort((a: {name: string}, b: {name: string}): any => {
+        // const x = a.name.toLowerCase();
+        // const y = b.name.toLowerCase();
+        // switch (this.order) {
+        //   case x > y:
+        //     return 1;
+        //     break;
+        //   case x < y:
+        //     return -1;
+        //     break;
+    //     }
+    //   });
+    //   return list;
+    // } else {
+    //   list.sort((a: {name: string}, b: {name: string}): any => {
+    //     const x = a.name.toLowerCase();
+    //     const y = b.name.toLowerCase();
+    //     switch (this.order) {
+    //       case x > y:
+    //         return 1;
+    //         break;
+    //       case x < y:
+    //         return -1;
+    //         break;
+    //     }
+    //   });
+    //   return list;
+    // }
   }
 
   toggleActivity() {
@@ -132,6 +125,68 @@ export class AppComponent implements OnInit {
       layer.style.transform = translate3d;
     }
   }
+
+  // toggleActivity() {
+  //   this.isChecked = !this.isChecked;
+  //   if (this.checkbox.nativeElement.checked === true) {
+  //     this.isChecked = true;
+  //     const activeList: Contact[] = this.cl.filter(c => c.active === true);
+  //     console.log(activeList);
+  //     return this.cl = activeList;
+  //   } else {
+  //     this.isChecked = false;
+  //     console.log(this.cl);
+  //     return this.cl;
+  //   }
+  // }
+
+  // sortBySurname() {
+  //   const aLSorted = this.al.sort((a: { surname: string }, b: { surname: string }) => {
+  //     const aLSurnameA = a.surname.toUpperCase();
+  //     const aLSurnameB = b.surname.toUpperCase();
+  //     if (aLSurnameA < aLSurnameB) { return -1; }
+  //     if (aLSurnameA > aLSurnameB) { return 1; }
+  //     return 0;
+  //   });
+
+  //   const clSorted = this.cl.sort((a: { surname: string }, b: { surname: string }) => {
+  //     const cLSurnameA = a.surname.toUpperCase();
+  //     const cLSurnameB = b.surname.toUpperCase();
+  //     if (cLSurnameA < cLSurnameB) { return -1; }
+  //     if (cLSurnameA > cLSurnameB) { return 1; }
+  //     return 0;
+  //   });
+
+  //   if (this.checkbox.nativeElement.checked === true) {
+  //     this.al = aLSorted;
+  //   } else {
+  //     this.cl = clSorted;
+  //   }
+  // }
+
+  // sortByCity() {
+  //   const aLSorted = this.al.sort((a: { city: string }, b: { city: string }) => {
+  //     const aLcityA = a.city.toUpperCase();
+  //     const aLCityB = b.city.toUpperCase();
+  //     if (aLcityA < aLCityB) { return -1; }
+  //     if (aLcityA > aLCityB) { return 1; }
+  //     return 0;
+  //   });
+
+  //   const clSorted = this.cl.sort((a: { city: string }, b: { city: string }) => {
+  //     const cLCityA = a.city.toUpperCase();
+  //     const cLCityB = b.city.toUpperCase();
+  //     if (cLCityA < cLCityB) { return -1; }
+  //     if (cLCityA > cLCityB) { return 1; }
+  //     return 0;
+  //   });
+
+  //   if (this.checkbox.nativeElement.checked === true) {
+  //     this.al = aLSorted;
+  //   } else {
+  //     this.cl = clSorted;
+  //   }
+  // }
 }
 
 // -------------------------------------------------------------------------
